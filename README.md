deeb9428ae56cb0a463a130dd2d9dca3be385a6b
Nathaniel Evan <nathanielevan@zohomail.com>
2022-07-28 17:14:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# homepage
+Custom browser homepage made by Nathaniel Evan. Inspired by various sources. Made with vanilla HTML-CSS-JS and designed with responsiveness in mind, based on Gruvbox Material colours by [@sainnhe](https://github.com/sainnhe).
+
+Feel free to fork and customise to your needs, or help me add new features or fix my incomprehensible code by submitting a pull request!
+
+## Demo
+
+![Demo](demo.mp4?raw=true)
---
+++ index.js
@@ -0,0 +1,39 @@
+document.onreadystatechange = updateClock()
+
+function updateClock() {
+    const date = new Date()
+    document.getElementById("current-time-text").innerText = date.toLocaleTimeString()
+    const hour = date.getHours()
+    if (hour < 5 || hour >= 18) {
+        document.getElementById("time-greeting").innerText = "Good evening."
+    }
+    else if (hour < 12) {
+        document.getElementById("time-greeting").innerText = "Good morning."
+    }
+    else {
+        document.getElementById("time-greeting").innerText = "Good afternoon."
+    }
+}
+
+setInterval(updateClock, 1000)
+
+function doSearch() {
+    const query = document.getElementById('search-input').value
+    switch (document.getElementById("search-engine-select").value) {
+        case 'google':
+            window.location.href = `https://www.google.com/search?q=${query}`
+            break;
+        case 'bing':
+            window.location.href = `https://www.bing.com/?q=${query}`
+            break;
+        case 'ddg':
+            window.location.href = `https://www.duckduckgo.com/?q=${query}`
+    }
+}
+
+const searchInput = document.getElementById("search-input")
+searchInput.addEventListener("keypress", function onEvent(event) {
+    if (event.key === "Enter") {
+        document.getElementById("search-button").click();
+    }
+});
