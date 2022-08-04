d3d33bb77c67da482ef3f6d0d36edf6746a8ca6a
Nathaniel Evan <nathanielevan@zohomail.com>
2022-08-04 13:26:06 +0700

    Change time format to 24-hour

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.onreadystatechange = updateClock()
 
 function updateClock() {
     const date = new Date()
-    document.getElementById("current-time-text").innerText = date.toLocaleTimeString()
+    document.getElementById("current-time-text").innerText = date.toLocaleTimeString([], { hour12: false })
     const hour = date.getHours()
     if (hour < 5 || hour >= 18) {
         document.getElementById("time-greeting").innerText = "Good evening."
